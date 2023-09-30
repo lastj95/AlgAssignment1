97aca41a94b4c23f0fd59dc778b3ccb2998f3608
Matthew Bertello <matthew.bertello@gmail.com>
2023-09-30 17:08:08 -0500

    Update the stock algorithm so it returns the buy/sell indices

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 import java.util.Random;
 import java.io.File;
 import java.io.FileNotFoundException;
+//import the Stock class from the Stock.java file
+
 
 /*
  * Program Authors: Matthew Bertello, James Last, Adeel Sultan
@@ -18,18 +20,17 @@ public class Main {
         // Asks user for input corresponding to one of the two problems.
         while (alg == 0) {
             System.out.print("Welcome to Assignment 1.\nEnter 1 for the stock problem, enter 2 for the tree problem, " +
-                             "or enter 3 for both: ");
+                    "or enter 3 for both: ");
             Scanner userIn = new Scanner(System.in);
             String input = userIn.nextLine();
             if (input.equals("1") || input.equals("2") || input.equals("3")) {
                 alg = Integer.parseInt(input);
-            }
-            else {
+            } else {
                 System.out.println("Error: Valid input not given.");
             }
         }
 
-        switch(alg) {
+        switch (alg) {
             case 1:
                 stockPriceProblemHelper();
                 break;
@@ -45,65 +46,100 @@ public class Main {
 
     }
 
-
     /*
-     * Generates the arrays of stock prices before calling the recursive algorithm to determine the max profit.
+     * Generates the arrays of stock prices before calling the recursive algorithm
+     * to determine the max profit.
      */
     public static void stockPriceProblemHelper() {
         // Generates the lists of stock prices.
-        for(int p=482; p<488; p++){
+        for (int p = 482; p < 488; p++) {
             Random r = new Random(p);
             int size = 10000000;
             int[] vals = new int[size];
-            for(int i=0; i<size; i++){
+            for (int i = 0; i < size; i++) {
                 vals[i] = r.nextInt(Integer.MAX_VALUE);
             }
-            // Calls the recursive method to print out the max profit for this stock price list.
-            System.out.println("Max profit for list " + p + ": " + stockPriceProblem(vals));
+            // Calls the recursive method to print out the max profit for this stock price
+            // list.
+            System.out.println("Buy index: " + stockPriceProblem(vals).buy);
+            System.out.println("Sell index: " + stockPriceProblem(vals).sell);
+            System.out.println("Max profit for list " + p + ": " + stockPriceProblem(vals).profit);
         }
     }
 
     /*
-     * Recursively gets the max profit from buying and selling once each in the array.
+     * Recursively gets the max profit from buying and selling once each in the
+     * array.
      */
-    public static int stockPriceProblem(int[] vals) {
+    public static Stock stockPriceProblem(int[] vals) {
         return GetMaxHelper(vals, 0, vals.length - 1);
     }
 
-    public static int GetMaxHelper(int[] arr, int start, int end) {
-        if (start >= end) {
-            return Integer.MIN_VALUE;
+    public static class Stock {
+        int buy;
+        int sell;
+        int profit;
+
+        public Stock getGreater(Stock other) {
+            if (this.profit > other.profit) 
+            {
+                return this;
+            } 
+            else 
+            {
+                return other;
+            }
         }
-        else if (end - start == 1) {
-            return arr[end] - arr[start];
+    }
+
+    public static Stock GetMaxHelper(int[] arr, int start, int end) {
+        Stock best = new Stock();
+        if (start >= end) {
+            best.buy = start;
+            best.sell = start;
+            best.profit = Integer.MIN_VALUE;
+            return best;
+        } else if (end - start == 1) {
+            best.buy = start;
+            best.sell = end;
+            best.profit = arr[end] - arr[start];
+            return best;
         }
 
         int mid = start + ((end - start) / 2);
-        int leftMax = GetMaxHelper(arr, start, mid);
-        int rightMax = GetMaxHelper(arr, mid + 1, end);
+        Stock leftMax = GetMaxHelper(arr, start, mid);
+        Stock rightMax = GetMaxHelper(arr, mid + 1, end);
 
         int leftCrossMin = arr[start];
         int rightCrossMax = arr[mid + 1];
+        int leftIndex = start;
+        int rightIndex = mid + 1;
 
-        for(int i = start + 1; i <= mid; i++) {
-            if(arr[i] < leftCrossMin) {
+        for (int i = start + 1; i <= mid; i++) {
+            if (arr[i] < leftCrossMin) {
                 leftCrossMin = arr[i];
+                leftIndex = i;
             }
         }
 
-        for(int i = mid + 2; i <= end; i++) {
-            if(arr[i] > rightCrossMax) {
+        for (int i = mid + 2; i <= end; i++) {
+            if (arr[i] > rightCrossMax) {
                 rightCrossMax = arr[i];
+                rightIndex = i;
             }
         }
 
-        int crossMax = rightCrossMax - leftCrossMin;
+        Stock crossMax = new Stock();
+        crossMax.buy = leftIndex;
+        crossMax.sell = rightIndex;
+        crossMax.profit = rightCrossMax - leftCrossMin;
 
-        return Math.max(Math.max(leftMax, rightMax), crossMax);
+        return leftMax.getGreater(rightMax).getGreater(crossMax);
     }
 
     /*
-     * Generates the list from a file called input4.txt from the InputFiles subdirectory. Then calls on
+     * Generates the list from a file called input4.txt from the InputFiles
+     * subdirectory. Then calls on
      * recursive method to get the majority-occurring element in the list.
      */
     public static void majorityTreeHelper() {
@@ -135,31 +171,31 @@ public class Main {
     }
 
     public static String findMajorityHelper(ArrayList<String> trees, int start, int end) {
-        if(start >= end) {
+        if (start >= end) {
             return trees.get(start);
         }
         int mid = start + ((end - start) / 2);
         String leftMajority = findMajorityHelper(trees, start, mid);
         String rightMajority = findMajorityHelper(trees, mid + 1, end);
-        if(java.util.Objects.equals(leftMajority, rightMajority)) {
+        if (java.util.Objects.equals(leftMajority, rightMajority)) {
             return leftMajority;
         }
         int leftCount = 0;
         int rightCount = 0;
 
-        for(int i = start; i <= end; i++) {
-            if(trees.get(i).equals(leftMajority)) {
+        for (int i = start; i <= end; i++) {
+            if (trees.get(i).equals(leftMajority)) {
                 leftCount++;
-            } else if(trees.get(i).equals(rightMajority)) {
+            } else if (trees.get(i).equals(rightMajority)) {
                 rightCount++;
             }
         }
 
         int majorityCount = (end - start + 1) / 2 + 1;
 
-        if(leftCount >= majorityCount) {
+        if (leftCount >= majorityCount) {
             return leftMajority;
-        } else if(rightCount >= majorityCount) {
+        } else if (rightCount >= majorityCount) {
             return rightMajority;
         } else {
             return null;
